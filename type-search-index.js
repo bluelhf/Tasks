d59f9975ee134d9a253d17bd8c39298296a1aa92
Blue <blueloveshisfriends@gmail.com>
2021-04-03 20:02:19 +0300

    Initial commit

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"io.github.bluelhf.tasks","l":"Task.Delegate"},{"p":"io.github.bluelhf.tasks","l":"Observable"},{"p":"io.github.bluelhf.tasks","l":"Task"}]
